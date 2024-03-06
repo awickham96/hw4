c4e26a6b1cf327620266f5360576bef027dbdf3f
Anna Wickham <anna.wickham@kellogg.northwestern.edu>
2024-03-06 23:18:58 +0000

    draft 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
+  def create 
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      if @user["password"] == params["password"]
+        flash["notice"] = "Welcome."
+        redirect_to "/"
+     else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+     end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
+    end
+    
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    redirect_to "/"
   end
 end
   
