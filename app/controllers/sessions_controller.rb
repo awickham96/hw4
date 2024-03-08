ca9e4a3cc87c2f901512347803cc660520a29b87
Anna Wickham <anna.wickham@kellogg.northwestern.edu>
2024-03-08 20:44:49 +0000

    hw draft 4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
   def destroy
     session["user_id"] = nil
     flash["notice"] = "Goodbye."
-    redirect_to "/"
+    redirect_to "/login"
   end
 end
   
