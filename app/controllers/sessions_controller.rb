6e5a1621b962c413a17d3d213b3210f0a7ad8d90
Anna Wickham <anna.wickham@kellogg.northwestern.edu>
2024-03-08 00:50:30 +0000

    hw 4 draft 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,22 +5,22 @@ class SessionsController < ApplicationController
   def create 
     @user = User.find_by({"email" => params["email"]})
     if @user != nil
-      if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome."
         redirect_to "/"
      else
-      flash["notice"] = "Nope."
+      flash["notice"] = "Try Again!"
       redirect_to "/login"
      end
     else
-      flash["notice"] = "Nope."
+      flash["notice"] = "Try Again!"
       redirect_to "/login"
     end
-    end
-    
   end
 
   def destroy
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/"
   end
